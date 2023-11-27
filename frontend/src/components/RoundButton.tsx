fdaf46299e40eb7d460fbbe4221f22d29eb1e073
Augusto Braga <bragaaugusto361@gmail.com>
2023-11-27 19:37:32 -0300

    feat(home): homepage finished

--- frontend/src/components/RoundButton.tsx
+++ frontend/src/components/RoundButton.tsx
@@ -10,8 +10,8 @@ interface ITransparentButtonProps {
 
 function RoundButton(props: ITransparentButtonProps) {
   const { text, link, target, styleButton } = props;
-  const classNames = `${styleButton} border-2 border-white pt-3 pb-3 pr-9 pl-9 rounded-full hover:bg-shaolinRed hover:text-black hover:pt-5 
-  hover:pb-5 hover:pl-11 hover:pr-11 hover:border-none duration-300`;
+  const classNames = `${styleButton} border-2 border-white pt-3 pb-3 pr-9 pl-9 rounded-full
+   hover:bg-shaolinRed hover:text-black hover:border-none duration-300`;
 
   return (
     <a href={link} target={target} className={classNames}>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "../index.css";
 import RoundButton from "../components/RoundButton";
+import logo from "../assets/Logo-Nova-Shaolin.png";
+import InstagramLogo from "../assets/Instagram-logo";
+import SpotifyLogo from "../assets/Spotify-logo";
+import FacebookLogo from "../assets/Facebook-logo";
+import YoutubeLogo from "../assets/Youtube-logo";
+import TiktokLogo from "../assets/Tiktok-logo";
+import TwitterLogo from "../assets/Twitter-logo";
+import TwitchLogo from "../assets/Twitch-logo";
 
 function HomePage() {
   const [message, setMessage] = useState("");
@@ -18,8 +27,8 @@ function HomePage() {
   }, []);
 
   return (
-    <div className="font-primary bg-black w-screen h-content flex flex-col justify-center">
-      <aside className="flex flex-col p-20 w-max">
+    <div className="font-primary bg-black w-screen h-content flex justify-between">
+      <aside className="flex flex-col p-20 w-max justify-center">
         <h2 className="text-shaolinRed text-6xl">SHAOLIN MONKEYS</h2>
         <h3 className="text-white text-2xl">CULTURA NERD E ROCK N' ROLL</h3>
         <div className="flex pt-10 pb-20 pr-10 text-white">
@@ -31,6 +40,60 @@ function HomePage() {
           <RoundButton text="SAIBA MAIS SOBRE NÓS" link="/band"></RoundButton>
         </div>
       </aside>
+      <div className="relative">
+        <img className="h-4/5 w-auto filter brightness-50" src={logo} alt="" />
+      </div>
+      <div className="absolute inset-y-0 right-5 flex flex-col items-center justify-center text-white">
+        <Link
+          to="https://open.spotify.com/artist/3VixRC25lOG8adkWJ5nWxC?si=8UIGaskwQlq1PZI8_DRMEg&nd=1"
+          target="_blank"
+          className="m-1"
+        >
+          <SpotifyLogo></SpotifyLogo>
+        </Link>
+        <Link
+          to="https://www.instagram.com/shaolinmonkeys/"
+          target="_blank"
+          className="m-1"
+        >
+          <InstagramLogo></InstagramLogo>
+        </Link>
+        <Link
+          to="https://www.facebook.com/shaolinmonkeysband"
+          target="_blank"
+          className="m-1"
+        >
+          <FacebookLogo></FacebookLogo>
+        </Link>
+        <Link
+          to="https://www.youtube.com/channel/UCBBq7NpCnV_tQA-6LbTF56Q"
+          target="_blank"
+          className="m-1"
+        >
+          <YoutubeLogo></YoutubeLogo>
+        </Link>
+        <Link
+          to="https://www.tiktok.com/@shaolinmonkeys"
+          target="_blank"
+          className="m-1"
+        >
+          <TiktokLogo></TiktokLogo>
+        </Link>
+        <Link
+          to="https://twitter.com/i/flow/login?redirect_after_login=%2Fmonkeysshaolin"
+          target="_blank"
+          className="m-1"
+        >
+          <TwitterLogo></TwitterLogo>
+        </Link>
+        <Link
+          to="https://www.twitch.tv/shaolinmonkeysgames"
+          target="_blank"
+          className="m-1"
+        >
+          <TwitchLogo></TwitchLogo>
+        </Link>
+      </div>
     </div>
   );
 }
